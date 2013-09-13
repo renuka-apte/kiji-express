7bcbdb2dc82d045cbe96ada3d1595757e75d50b6
Renuka Apte <renuka@wibidata.com>
2013-09-12 19:42:35 -0700

    mostly done LR

---
+++ src/main/scala/org/kiji/express/modeling/lib/LMTrainer.scala
@@ -0,0 +1,114 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kiji.express.modeling.lib
+
+import org.kiji.express.modeling.{KeyValueStore, Trainer}
+import com.twitter.scalding.{Tsv, TextLine, Source}
+import org.kiji.express.KijiSlice
+import collection.mutable.ArrayBuffer
+import org.kiji.express.flow.KijiSource
+
+class LMTrainer(numFeatures:Int, learningRate: Double, epsilon: Double, maxIterations: Int) extends Trainer {
+  /**
+   *
+   * @param attr
+   * @return
+   */
+  def vectorizeDataPoint(attr: KijiSlice[Double]): IndexedSeq[Double] = {
+    // TODO Decide how the input source will look like for data with more than one attribute.
+    val attributes  = new ArrayBuffer[Double]
+    attributes.append(1.0, attr.getFirstValue())
+    attributes.toIndexedSeq
+  }
+
+  /**
+   *
+   * @param thetas is the source containing the currently calculated parameters for linear
+   *    regression.
+   * @return an ordered sequence of doubles representing the thetas.
+   */
+  def vectorizeParameters(thetas: Source): IndexedSeq[Double] = {
+    thetas.readAtSubmitter[String]
+        .map((line: String) => {
+          val lineComponents = line.split("\t")
+          (lineComponents(0).toInt, lineComponents(1).toDouble)
+        })
+        .sortWith((a,b) => a._1.compareTo(b._1) < 0)
+        .map(paramTuple => paramTuple._2)
+        .toIndexedSeq
+  }
+
+  class LMJob (input: Map[String, Source], output: Map[String, Source],
+      parameters:IndexedSeq[Double]) extends TrainerJob {
+
+    val datasetSource:KijiSource = input.getOrElse("dataset", null).asInstanceOf[KijiSource]
+
+    datasetSource.mapTo(('attributes, 'target) -> 'gradient) {
+      case (xvalue: KijiSlice[Double], yvalue: KijiSlice[Double]) => {
+        val attributes: IndexedSeq[Double] = vectorizeDataPoint(xvalue)
+        val target: Double = yvalue.getFirstValue()
+
+        // y - (theta)'(X)
+        val delta: Double = target - parameters.zip(attributes).map(x => x._1 * x._2).reduce(_+_)
+
+        // TODO: May need to convert this into a tuple (attributes(0), attributes(1),......)
+        attributes.map(x => x * delta)
+      }
+      case _ => throw new IllegalArgumentException("Mismatch in type of dataset/target.")
+    }
+    // index thetas by position
+    .flatMapTo('gradient -> ('index, 'indexedGradient)) {
+      gradient: IndexedSeq[Double] => gradient.zipWithIndex.map{ x =>(x._2, x._1) }
+    }
+    // calculating new thetas
+    .groupBy('index) {
+      _.reduce('indexedGradient -> 'totalIndexedGradient) {
+        (gradientSoFar : Double, gradient : Double) => gradientSoFar + gradient
+      }
+    }
+    // update old theta depending on learning rate and convert thetas to a string
+    .mapTo(('index, 'totalIndexedGradient) -> 'indexedGradientString) {
+      gradientTuple: (Int, Double) => {
+       gradientTuple._1.toString  + "\t" + (parameters(gradientTuple._1) +
+          (learningRate * gradientTuple._2)).toString
+      }
+    }
+    .write(output.getOrElse("parameters", null))
+  }
+
+  /*
+  def converged(oldParams: IndexedSeq[Double], newParams: IndexedSeq[Double]): Boolean = {
+    oldParams.zip(newParams)
+      .map(params => math.abs(params._1 - params._2))
+      .reduce(_+_)/(numFeatures + 1) < epsilon
+  }
+  */
+
+  override def train(input: Map[String, Source], output: Map[String, Source]): Boolean = {
+    // Read the parameters (thetas) from a text file on hdfs and convert them into a sequence
+    // of doubles that can be consumed by a job.
+    val parameterSource:TextLine = input.getOrElse("parameters", null).asInstanceOf[TextLine]
+    val parameters:IndexedSeq[Double] = vectorizeParameters(parameterSource)
+
+    new LMJob(input, output, parameters).run
+
+    true
+  }
+}
---
+++ src/main/scala/org/kiji/express/tool/LMTool.scala
@@ -0,0 +1,108 @@
+/**
+ * (c) Copyright 2013 WibiData, Inc.
+ *
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.kiji.express.tool
+
+import org.kiji.annotations.{ApiAudience, ApiStability}
+import com.twitter.scalding.Tool
+import org.kiji.express.modeling.config._
+import org.kiji.express.modeling.lib.LMTrainer
+import org.kiji.express.modeling.config.ExpressColumnRequest
+import org.kiji.express.modeling.config.ExpressDataRequest
+import scala.Some
+import org.kiji.express.modeling.framework.ModelExecutor
+import org.slf4j.Logger
+import org.apache.hadoop.util.ToolRunner
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.slf4j.LoggerFactory
+
+
+@ApiAudience.Private
+@ApiStability.Experimental
+final class LMTool extends Tool{
+
+  override def run(args: Array[String]): Int = {
+    val datasetTableURI: String = args(0)
+    val paramsFilePath: String = args(1)
+
+    val modelDefinition: ModelDefinition = ModelDefinition(
+      name = "lm-model-def",
+      version = "1.0",
+      trainerClass = Some(classOf[LMTrainer]))
+
+    val request: ExpressDataRequest = new ExpressDataRequest(0, Long.MaxValue, Seq(
+      new ExpressColumnRequest("attributes", 1, None),
+      new ExpressColumnRequest("target", 1, None)
+    ))
+
+    val modelEnvironment: ModelEnvironment = ModelEnvironment(
+      name = "lm-model-environment",
+      version = "1.0",
+      prepareEnvironment = None,
+      trainEnvironment = Some(TrainEnvironment(
+        inputSpec = Map(
+          "dataset" ->
+            KijiInputSpec(datasetTableURI,
+                          dataRequest = request,
+                          fieldBindings = Seq(
+                            FieldBinding(tupleFieldName = "attributes",
+                                          storeFieldName = "attributes"),
+                            FieldBinding(tupleFieldName = "target",
+                                          storeFieldName = "target")))),
+          /*"parameters" -> None
+            /*TextFileInputSpec*/),*/
+        outputSpec = Map(
+          /*"parameters" ->
+          TextFileOutputSpec*/),
+        keyValueStoreSpecs = Seq()
+      )),
+      scoreEnvironment = None
+    )
+    // Build the produce job.
+    val modelExecutor = ModelExecutor(modelDefinition, modelEnvironment)
+    modelExecutor.runTrainer() match {
+      case true => 0
+      case false => 1
+    }
+  }
+}
+
+/*
+ * The companion object to ScoreJobTool that only contains a main method.
+ */
+object LMTool {
+  val LOGGER: Logger = LoggerFactory.getLogger(LMTool.getClass)
+  /**
+   * The entry point into the tool.
+   *
+   * @param args from the command line.
+   * @return a return code that signals the success of the specified job.
+   */
+  def main(args: Array[String]) {
+
+    if (args.length < 2){
+      println("Usage: LMTool <dataset-table-URI> <parameters-file-path>")
+      System.exit(1)
+    }
+
+    ToolRunner.run(HBaseConfiguration.create(), new LMTool, args)
+  }
+
+}
